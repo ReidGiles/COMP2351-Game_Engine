b68a6419c1724df934c0f1a849a9045d641a3991
Reid Giles <33459088+ReidGiles@users.noreply.github.com>
2019-01-26 13:15:18 +0000

    Initial

---
+++ COMP2351-Game_Engine/COMP2351-Game_Engine/RelicHunterEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace COMP2351_Game_Engine
+{
+    abstract class RelicHunterEntity: Entity
+    {
+        public Texture2D texture;
+        public Vector2 location;
+        public Rectangle HitBox
+        {
+            get
+            {
+                Rectangle hitBox = new Rectangle((int)this.location.X, (int)this.location.Y, texture.Width, texture.Height);
+                return hitBox;
+            }
+        }
+        public RelicHunterEntity()
+        {
+
+        }
+    }
+}
