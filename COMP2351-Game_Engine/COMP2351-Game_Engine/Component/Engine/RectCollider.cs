84d0bc81802c4cca3fcf2e1ac8bea86d2acfb04a
MTTownsend <33583672+MTTownsend@users.noreply.github.com>
2019-03-21 13:06:51 +0000

    Created Colliders
    Created a rectangle collider and required interfaces to update it and for the manager to create the collision area

---
+++ COMP2351-Game_Engine/COMP2351-Game_Engine/Component/Engine/RectCollider.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP2351_Game_Engine
+{
+    class RectCollider : ICollider,ICreateCollider
+    {
+        //origin point of the rectangle collider
+        private Vector2 origin;
+
+        //width of the collider
+        private float width;
+
+        //height of the collider
+        private float height;
+
+
+        public RectCollider(Vector2 pOrigin, float pWidth, float pHeight)
+        {
+            //set origin
+            origin = pOrigin;
+
+            //set width
+            width = pWidth;
+
+            //set height
+            height = pHeight;
+        }
+
+        //Translate the position of the Collider
+        public void Translate(float dx, float dy)
+        {
+            //move x
+            origin.X += dx;
+            //move y
+            origin.Y += dy;
+        }
+
+        //get the origin point of the collider
+        public Vector2 GetOrgin()
+        {
+            return origin;
+        }
+
+        //get the width and height of the collider
+        public float[] GetSize()
+        {
+            float[] retVal = { width, height};
+            return retVal;
+        }
+    }
+}
---
+++ COMP2351-Game_Engine/COMP2351-Game_Engine/Interface/Engine/ICreateCollider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP2351_Game_Engine
+{
+    interface ICreateCollider
+    {
+        //get the origin point of the collider
+        Vector2 GetOrgin();
+
+        //get the width and height of the collider
+        float[] GetSize();
+
+    }
+}
